99aed7672c83a62f6b68d94803a2869abadfdf0a
monkey <70706886+a3510377@users.noreply.github.com>
2021-05-23 19:24:23 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='color',
+    version='0.0.1',
+    author='xlzd',
+    author_email='a102009102009@gmail.com',
+    url='https://zhuanlan.zhihu.com/p/26159930',
+    description=u'有色文字',
+    packages=['color'],
+    install_requires=[],
+    entry_points={
+        'console_scripts': []
+    }
+)
